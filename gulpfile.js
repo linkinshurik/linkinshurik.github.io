b306f18da34dde839706931080e0acbef133accb
Oleksandr Bezobrazov <alexandr.bezobrazov@gmail.com>
2019-02-19 11:22:54 +0200

    initial

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var validator = require('gulp-html');
+
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('html', function() {
+    return gulp.src('src/index.html')
+    .pipe(validator())
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+    gulp.watch('sass/*.scss', gulp.series('sass'));
+});
+
+gulp.task('html:watch', function () {
+    gulp.watch('src/*.html', gulp.series('html'));
+});
+
+gulp.task('default', gulp.parallel('html:watch', 'sass:watch'));
